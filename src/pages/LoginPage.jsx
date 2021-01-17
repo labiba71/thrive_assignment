49cb0534bdd70ee10da8d533c7bff7ebe82a71b5
Labiba Ibnat <labiba.ssp@gmail.com>
2021-01-17 22:52:51 +0600

    login page done

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import newtonIcon from "../resources/newton.svg";
+import bookIcon from "../resources/book.png";
+import { userLogin } from "../store/Action.js";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import "./LoginPage.css";
+
+const LoginPageComponent = (props) => {
+  return (
+    <div className="loginMain">
+      <img src={newtonIcon} alt="icon" />
+      <img className="bookImg" src={bookIcon} alt="icon" />
+      <p>
+        Please find the ID and PIN from SMS. Don’t share your ID or PIN with
+        anyone.
+      </p>
+      <div className="innerDiv">
+        <label>
+          ID:
+          <input placeholder="Enter your ID" type="text" id="id" name="id" />
+        </label>
+        <label>
+          PIN:
+          <input
+            placeholder="Enter your PIN"
+            type="password"
+            id="pin"
+            name="pin"
+          />
+        </label>
+        <button
+          className="loginBtn"
+          onClick={(e) => {
+            e.preventDefault();
+            props.userLogin();
+            props.history.push("/profile");
+          }}
+        >
+          Log In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const LoginPage = connect(null, { userLogin })(
+  withRouter(LoginPageComponent)
+);
